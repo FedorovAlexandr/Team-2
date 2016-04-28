de8be6c6a49f168aa26a37cd61e88f8cca464ae2
FedorovAlexandr <al_fedorov@icloud.com>
2016-04-28 21:00:55 +0300

    Update test_jenkins.py

--- test_jenkins.py
+++ test_jenkins.py
@@ -10,21 +10,16 @@ def int_to_words(n):
     # 1000+
     for order, word in [(10**6, "million"), (10**3, "thousand")]:
         if n >= order:
-            return "{0} {1}{2}".format(int_to_words(n // order), word,
-                                       " {0}".format(int_to_words(n % order))
-                                       if n % order else "")
+            return sys.exit(0)
     # 100-999
     if n >= 100:
         if n % 100:
-            return "{0} hundred and {1}".format(int_to_words(n // 100),
-                                                int_to_words(n % 100))
+            return sys.exit(0)
         else:
-            return "{0} hundred".format(int_to_words(n // 100))
+            return sys.exit(0)
     # 0-99
     if n < 20:
-        return ones[n]
+        return sys.exit(0)
     else:
-        return "{0}{1}".format(tens[n // 10],
-                               "-{0}".format(int_to_words(n % 10))
-                               if n % 10 else "")
-int_to_words(44)
+        return sys.exit(0)
+
