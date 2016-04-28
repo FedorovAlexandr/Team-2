b29b160f1931686b24d18ac4d905e00bf85c6e8d
FedorovAlexandr <al_fedorov@icloud.com>
2016-04-28 21:03:34 +0300

    Update test_jenkins.py

--- test_jenkins.py
+++ test_jenkins.py
@@ -1,25 +1,5 @@
-def int_to_words(n):
-    """Convert an integer n to English words."""
-
-    ones = ['zero', 'one', 'two', 'three', 'four',
-            'five', 'six', 'seven', 'eight', 'nine',
-            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-    tens = ['zero', 'ten', 'twenty', 'thirty', 'forty',
-            'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-    # 1000+
-    for order, word in [(10**6, "million"), (10**3, "thousand")]:
-        if n >= order:
-            return sys.exit(0)
-    # 100-999
-    if n >= 100:
-        if n % 100:
-            return sys.exit(0)
-        else:
-            return sys.exit(0)
-    # 0-99
-    if n < 20:
-        return sys.exit(0)
-    else:
-        return sys.exit(0)
+def test(x):
+    if x > 10:
+        print "awesome"
+    return sys.exit(0)
 
